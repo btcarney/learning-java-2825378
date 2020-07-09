10009b484fac42e1c3614926c79ae501bb6370d1
Barney Carney <Barney.Carney@childrens.harvard.edu>
2020-07-09 13:15:27 -0400

    wrap this up!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.4;
+        String firstName = "Barney";
+        String middleName = "Thomas";
+        char firstInit = firstName.charAt(0);
+        char midInit = middleName.charAt(0);
+        boolean hasPerf = true;
+        String lastName = "Carney";
+        String fullName = lastName + ", " + firstInit + ". " + midInit + ".";
+        System.out.println("The student's age is " + studentAge + " and their Full Name is " + fullName);
     }
 }
