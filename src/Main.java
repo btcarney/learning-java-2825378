583544833c4ad99b1004a99b40b554e6d21a787d
Barney Carney <Barney.Carney@childrens.harvard.edu>
2020-07-10 09:57:44 -0400

    Thanks for clarifying equals() vs ==

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("The current song is playing. Do you want to go on to the next song?");
+            String WhatDoYouWant = input.next();
+            String lc = WhatDoYouWant.toLowerCase();
+            if (lc.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
     }
 }
