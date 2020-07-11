c40cc5682e7da9866238319bc6d9b9d42370c413
Barney Carney <Barney.Carney@childrens.harvard.edu>
2020-07-11 15:47:42 -0400

    thanks

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,16 @@ public class Main {
 
         String correctAnswer;
         correctAnswer = choiceTwo;
-
+        Scanner input = new Scanner(System.in);
         // Write a print statement asking the question
         System.out.println(question);
         // Write a print statement giving the answer choices
-        System.out.println("Possible Answers are " + choiceOne + "or " + choiceTwo + " or " + choiceThree + ". What's your guess?");
+        System.out.println("Possible Answers are " + choiceOne + " or " + choiceTwo + " or " + choiceThree + ". What's your guess?");
         // Have the user input an answer
         String guess;
         guess = input.next();
         while (!guess.equals(correctAnswer)) {
-            System.out.println("Sorry, possible Answers are " + choiceOne + "or " + choiceTwo + " or " + choiceThree + ". What's your guess?");
+            System.out.println("Sorry, possible Answers are " + choiceOne + " or " + choiceTwo + " or " + choiceThree + ". What's your guess?");
             guess = input.next();
         }
         System.out.println("You got it.");
